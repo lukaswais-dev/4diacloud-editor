86ae128d02147b8df4356b4b40a7834c88d8276e
Lukas Wais <lukas.wais@outlook.de>
2021-09-17 11:41:40 +0200

    Created Webview

--- src/extension.ts
+++ src/extension.ts
@@ -10,17 +10,35 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "4diacloud-editor" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('4diacloud-editor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from 4diacloud-editor!');
-	});
+	context.subscriptions.push(
+		vscode.commands.registerCommand('4diacloud-editor.start', () => {
+		  // Create and show a new webview
+		  const panel = vscode.window.createWebviewPanel(
+			'4diacloud', // Identifies the type of the webview. Used internally
+			'4diacloud Editor', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		  );
+		  // set the HTML content
+		  panel.webview.html = getWebviewContent();
+		})
+	  );
+}
 
-	context.subscriptions.push(disposable);
+function getWebviewContent() {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none';">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>4diacloud Editor</title>
+  </head>
+  <body>
+  <h1>Hello Webview</h1>
+   </body>
+  </html>`;
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+// export function deactivate() {}
