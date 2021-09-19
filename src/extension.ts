7c8c27196a89a0f51014728e01f5f42a9ddb762d
Lukas Wais <lukas.wais@outlook.de>
2021-09-19 12:41:44 +0200

    Enabled local file loading

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -16,46 +17,62 @@ export function activate(context: vscode.ExtensionContext) {
 				'4diacloud', // Identifies the type of the webview. Used internally
 				'4diacloud Editor', // Title of the panel displayed to the user
 				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-				{} // Webview options. More on these later.
+				{
+					// Enable scripts in the webview
+					enableScripts: true,
+					localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, '../src/editor/'))] // only files of inside the editor directory are loaded
+				} // Webview options. More on these later.
 			);
+			const htmlOnDiskPath = vscode.Uri.file(path.join(context.extensionPath, '../src/editor/', 'diagram.html'));
+			const htmlOnDisk = panel.webview.asWebviewUri(htmlOnDiskPath);
+
+			
+
 			// set the HTML content
-			panel.webview.html = getWebviewContent();
+			panel.webview.html = getWebviewContent(htmlOnDisk);
 		})
 	);
 }
 
-function getWebviewContent() {
-	// return `
-	// <svg height="210" width="500">
-  	// 	<polygon points="100,10 40,198 190,78 10,78 160,198" style="fill:lime;stroke:purple;stroke-width:5;fill-rule:evenodd;"/>
-  	// 	Sorry, your browser does not support inline SVG.
-	// </svg>
-	// `;
-
-	let name:string = "Hello from a TS Variable"; 
+function getWebviewContent(htmlOnDisk: vscode.Uri) {
+	let name: string = "Hello from a TS Variable";
 	return `
 	<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cat Coding</title>
-</head>
-<body>
-	<h1>Hello ${name}</h1>
-    <h1 id="lines-of-code-counter">0</h1>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Cat Coding</title>
+		</head>
+		<body>
+			<h1>Hello ${name}</h1>
+			<h1 id="lines-of-code-counter">0</h1>
 
-    <script>
-        const counter = document.getElementById('lines-of-code-counter');
+			<script>
+				const counter = document.getElementById('lines-of-code-counter');
 
-        let count = 0;
-        setInterval(() => {
-            counter.textContent = count++;
-        }, 100);
-    </script>
-</body>
-</html>
-	`;
+				let count = 0;
+				setInterval(() => {
+					counter.textContent = count++;
+				}, 100);
+			</script>
+			<svg width="400" height="100">
+				<rect width="400" height="100" style="fill:rgb(0,0,255);stroke-width:10;stroke:rgb(0,0,0)" />
+			</svg>
+
+			<svg height="130" width="500">
+				<defs>
+					<linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+					<stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+					<stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+					</linearGradient>
+				</defs>
+				<ellipse cx="100" cy="70" rx="85" ry="55" fill="url(#grad1)" />
+				<text fill="#ffffff" font-size="45" font-family="Verdana" x="50" y="86">SVG</text>
+			</svg>
+			<a href="${htmlOnDisk}"> abc </a>
+		</body>
+	</html>`;
 }
 
 // this method is called when your extension is deactivated
