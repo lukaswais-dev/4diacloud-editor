8ba875a4adddcf5da905a2298275185ebc012cd2
Lukas Wais <lukas.wais@outlook.de>
2021-10-29 00:28:18 +0200

    sample with gojs

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import * as path from 'path';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -8,8 +9,33 @@ export function activate(context: vscode.ExtensionContext) {
         'fourdiac', // Identifies the type of the webview. Used internally
         '4diac ide', // Title of the panel displayed to the user
         vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-        {} // Webview options. More on these later.
+        {
+          // Enable scripts in the webview
+          enableScripts: true,
+        }
       );
+      const scriptUri = vscode.Uri.file(path.join(context.extensionPath, '4diac', 'graph.js'));
+      const scriptSrc = panel.webview.asWebviewUri(scriptUri);
+      // And set its HTML content
+      panel.webview.html = getWebviewContent(scriptSrc);
     })
   );
+}
+
+function getWebviewContent(scriptUri: vscode.Uri) {
+  console.log(scriptUri);
+  return `<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://unpkg.com/gojs/release/go-debug.js"></script>
+</head>
+
+<body>
+  <div id="myDiagramDiv" style="width:400px; height:150px; background-color: #DAE4E4;"></div>
+  <script src="${scriptUri}"></script>
+</body>
+
+</html>`;
 }
