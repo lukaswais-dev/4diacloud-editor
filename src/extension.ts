290ea2168856d2aa8fbb257623b3d4d3c3c3c771
Lukas Wais <lukas.wais@outlook.de>
2021-10-28 11:50:50 +0200

    Updated correct main JS file

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,10 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand('4diac.start', () => {
+    vscode.commands.registerCommand('fourdiac.start', () => {
       // Create and show a new webview
       const panel = vscode.window.createWebviewPanel(
-        '4diac', // Identifies the type of the webview. Used internally
+        'fourdiac', // Identifies the type of the webview. Used internally
         '4diac ide', // Title of the panel displayed to the user
         vscode.ViewColumn.One, // Editor column to show the new webview panel in.
         {} // Webview options. More on these later.
